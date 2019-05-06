49293020111b0c33fd2f8f7a9db3f293c152b135
DeLoMit522 <50342958+DeLoMit522@users.noreply.github.com>
2019-05-06 17:51:10 -0500

    Add files via upload

--- hw11_simulation.R
+++ hw11_simulation.R
@@ -1,6 +1,17 @@
-generate_data <- function(n,p){
-  infoForMatrix <- rnorm(n*p)
-  covariates <- matrix(data = infoForMatrix, nrow = n, ncol = p)
-  responses <- rnorm(n)
-  return(c(covariates, responses))
+generate_data = function(n, p){
+  whatToReturn = list()
+  covariates = matrix(nrow = n, ncol = p)
+  for(i in 1:p){
+    covariates[,i] = rnorm(p)
+  }
+  responses = rnorm(n)
+  whatToReturn$covariates = covariates
+  whatToReturn$responses = responses
+  return(whatToReturn)
+}
+
+model_select = function(covariates, responses, cutoff){
+  firstModel = lm(responses ~ covariates, summary(lm(responses~covariates))[[4]][2,4] <= cutoff)
+  secondModel = lm(responses ~ covariates[, summary(lm(responses~covariates))[[4]][2,4] <= cutoff])
+  return(summary(secondModel)[[4]][2,4])
 }
