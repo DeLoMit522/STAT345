8809e68b0982fbb65c32401923559ac2804ad9dd
DeLoMit522 <50342958+DeLoMit522@users.noreply.github.com>
2019-05-06 18:06:57 -0500

    Add files via upload

--- hw11_simulation.R
+++ hw11_simulation.R
@@ -18,7 +18,18 @@ model_select = function(covariates, responses, cutoff){
 
 run_simulations = function(n_trials, n, p, cutoff){
   for(i in 1:n_trials){
-    stuff = generate_data(n, p)
-    hist(model_select(stuff$covariates, stuff$responses, cutoff))
+    stuff <- generate_data(n, p)
+    #hist(model_select(stuff$covariates, stuff$responses, cutoff))
+  }
+}
+
+make_plot(datapath){
+  data <- readLines(datapath)
+  hist(data)
+}
+
+for(nn in c(100, 1000, 10000)){
+  for (pp in c(10, 20, 50)){
+    run_simulations(1, nn, pp, cutoff = 0.05)
   }
 }
